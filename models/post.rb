cb23c9d4d76a9e486444b337f45338278360197f
Ausin Formica <austin.formica@gmail.com>
2014-12-17 14:59:10 -0500

    tag objects work

--- models/post.rb
+++ models/post.rb
@@ -5,5 +5,7 @@ class Post < ActiveRecord::Base
 	# validates_presence_of :body, :user_id, presence: true
 	belongs_to :user
 	has_many :comments
+	has_many :taggings
+  	has_many :tags, through: :taggings
 
 end
